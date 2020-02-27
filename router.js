9e8e89de71abde833cea15124865ed9ee57f19de
Cosmin Ivanov <mcivanov99@gmail.com>
2020-02-27 22:30:06 +0200

    facut templateuri functionale

--- router.js
+++ router.js
@@ -2,11 +2,32 @@ let url = require('url');
 let qs = require('querystring');
 let fs = require('fs');
 
-let sendTemplate = (req, res, path, statusCode) => {
+
+// the function that parses the template
+let prepareServita = (templateBody, context) => {
+    // TODO sa se faca tempalteuri care sa poata si evalua anumite expresii sau un for :P
+    
+    let parsedResult = String(templateBody);
+    let objectFindRegEx = /\{\{(?<objdata>.*?)\}\}/g; // this regex finds words inside {{ }}
+
+    searchResult = objectFindRegEx.exec(parsedResult);
+    
+    if(searchResult == null) { // there is nothing more to be replaced
+        return parsedResult;
+    }
+
+    parsedResult = parsedResult.replace(searchResult[0], context[searchResult[1].trim()]);
+    return prepareServita(parsedResult, context);
+
+}
+
+// context represents a object that contains the variables that will be
+// used in the template
+let sendTemplate = (req, res, path, context, statusCode) => {
     res.writeHead(statusCode, {'Content-type': 'text/html'})
     let content = fs.readFileSync(path);
-    res.end(content)
-    // TODO fa chiar ca templateurile sa fie templateuri
+    
+    res.end(prepareServita(content, context));
 }
 
 
@@ -28,9 +49,12 @@ let resolver = (req, res) => {
         requestInfo.body = qs.parse(requestBody);
         requestInfo.urlObject = urlObject;
         // TODO facut functie separata pentru popularea requestInfo
-
+        let testContext = {
+            "gheiString": "Paul ultra ghei", 
+            "orNot" : "E doar o gluma ca sa demonstrez templateurile :)"
+        };
         if(requestInfo.urlPathname == '/') {
-            sendTemplate(req, res, "index.html", 200);
+            sendTemplate(req, res,"index.html", testContext, 200);
         }
         else {
             res.writeHead(200, {'Content-type':'application/json'});
