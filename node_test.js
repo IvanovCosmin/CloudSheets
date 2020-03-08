6a66b4a21fbd78ef35d9feda855ab5b95d8ed01e
Paul1221 <paul_man70@yahoo.com>
2020-03-08 23:20:54 +0200

    scuze cos am uitat de push. Am facut un obiect pentru baza de date si am facut modificari pt a fi folosit acel obiect in loc de ce aveam in db_scripts

--- node_test.js
+++ node_test.js
@@ -3,7 +3,7 @@ let fs = require('fs');
 let config = require('./config'); 
 let router = require('./router');   
 let oauth2orize = require('oauth2orize');
-
+let db=require('./database');
 let oauthServer = oauth2orize.createServer();
 
 
@@ -12,18 +12,26 @@ let options = {
     cert: fs.readFileSync(config['certpath'])
 };
 
-
 server = https.createSecureServer(options, router.resolve);
+bazadate= db.DataBase();
 server.on('session', (session) => {
     session.on('connect', (socket) => {
         // console.log(session);
-        // console.log(socket);
+        // console.Slog(socket);
         console.log("a client has connected");
     });
-});
+    
 
+});
 
 server.listen(config['port']);
 console.log("listening on port " + config['port']);
 
+process.on('SIGINT', () => {
+    bazadate.closeDatabase()
+  })
 
+
+module.exports={
+    bazadate
+};
