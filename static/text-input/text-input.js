5adffece24a2e1fd9f41e7ba722ed0c3e3f4f542
Paul1221 <paul_man70@yahoo.com>
2020-05-08 21:27:21 +0300

    testare autentificare

--- static/text-input/text-input.js
+++ static/text-input/text-input.js
@@ -1,6 +1,23 @@
 
+ form = document.getElementById("form");
+ username = document.getElementById("username");
+ email = document.getElementById("email");
+ password = document.getElementById("password");
+ cpassword = document.getElementById("second_password");
 
-function onSubmit(username,email){
+ function checkInputs(){
     
 }
 
+form.addEventListener('submit',(e)=>{
+
+    e.preventDefault();
+    
+    if(cpassword.value !== password.value){
+        
+    }
+    else{
+       e.currentTarget.submit();
+       //window.location.href=`https://localhost:8000/text-input/homepage`
+    }
+});
