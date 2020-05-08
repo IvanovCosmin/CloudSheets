44ced667299545af53945423c7b74da0297fab80
Paul1221 <paul_man70@yahoo.com>
2020-05-09 01:10:42 +0300

    legat front end de back end pt autentificare + modificari necesare front

--- static/welcomePage/button-action.js
+++ static/welcomePage/button-action.js
@@ -5,44 +5,58 @@ function changeColor(idName1, idName2) {
     document.getElementById(idName2).style.background="white"
   }
 
- const getStarted = `<form >
+ const getStarted = `<form action="/welcomePage/onRegister" method="POST" >
 
-<label for="fname" class="labelTitle">First Name</label>
-<input type="text" id="fname" name="firstname" placeholder="Your name..">
+<label for="name" class="labelTitle">Name:</label>
+<input type="text" name="name" id="name" placeholder="Your first name.." minlength="3" maxlength="12"
+       pattern="[a-zA-Z][a-zA-Z_]*[a-zA-Z]"
+       title="The name must contain only letters."
+       required> 
 
-<label for="lname" class="labelTitle">Last Name</label>
-<input type="text" id="lname" name="lastname" placeholder="Your last name..">
-
-<label for="email" class="labelTitle">Email</label>
-<input type="text" id="email" name="email" placeholder="Your email..">
-
-<label for="password" class="labelTitle">Password</label>
-<input type="password" id="passsword" name="password" >
-
-<label for="passwordConfirm" class="labelTitle">Confirm password</label>
-<input type="password" id="passwordC" name="passwordC" >
+<label for="surname" class="labelTitle">Surname:</label>
+<input type="text" name="surname" id="surname" placeholder="Your last name.." minlength="3" maxlength="12"
+       pattern="[a-zA-Z][a-zA-Z_]*[a-zA-Z]"
+       title="The surname must contain only letters."
+       required> 
 
 
+<label for="email" class="labelTitle" >Email address:</label>
+<input type="text" name="email" id="email" placeholder="Enter your email.." minlength="5" maxlength="90"
+       pattern="^[a-zA-Z0-9.!#$%&'*+/=?^_{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+       title="Please enter a valid email address."
+       required>
+
+<label for="password" class="labelTitle" >Choose a password:</label>
+<input type="password" name="password" id="password" placeholder="Enter your password.." minlength="7" maxlength="30" 
+       pattern=^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$
+       title="Please include at least 1 uppercase character, 1 lowercase character, and 1 number." 
+       required>
+
+<label for="cpassword" class="labelTitle">Confirm your password:</label>
+<input type="password" name="cpassword" id="cpassword" placeholder="Enter your password.." minlength="7" maxlength="30" 
+       pattern=^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$
+       title="Please include at least 1 uppercase character, 1 lowercase character, and 1 number." 
+       required>
+
+       <input type="submit"  id="submitBtn" value="Submit">
 </form>
-<input type="submit" onclick="submitBtn()" id="submitBtn" value="Submit">`;
+`;
 
-const logIn=`<form >
+const logIn=`<form action="/welcomePage/onLogin" method="POST" >
 
 
-<label for="email" class="labelTitle">Email</label>
-<input type="text" id="email" name="lastname" placeholder="Your email..">
+<label for="email">Email:</label>
+<input type="text" name="email" id="email" placeHolder="Enter your email.." required > 
 
-<label for="password" class="labelTitle">Password</label>
-<input type="password" id="password" name="password" >
+<label for="password">Password:</label>
+<input type="password" name="password" id="password" placeHolder="Enter your password.." required>
 
+<input type="submit"  id="submitBtn1" value="Submit">
 </form>
-<input type="submit" onclick="submitBtn()" id="submitBtn1" value="Submit">`;
+`;
 
 function submitBtn() {
-  // document.getElementById("submitBtn1").onclick = function () {
-    console.log("heeelllpppp");
-    location.href = "../mainScreen/index.html";
-// };
+  
   
 }
 
